5b4e20bcfc18279c670c35f9eb0f11a809c1de4c
Chris Thoburn <runspired@gmail.com>
2015-07-10 16:50:05 -0500

    computed state and default register action

--- addon/components/validated-area.js
+++ addon/components/validated-area.js
@@ -1,11 +1,33 @@
 import Ember from 'ember';
 import layout from '../templates/components/validated-area';
 
-export default Ember.Component.extend({
+const {
+  Component,
+  computed
+  } = Ember;
+
+export default Component.extend({
+
   layout: layout,
 
   errors: null,
+
   valid: null,
+
+  state: computed('valid', function() {
+
+    let valid = this.get('valid');
+    let state = {};
+
+    state.isValid = valid === true;
+    state.isInvalid = valid === false;
+    state.isInitial = valid === null;
+    state.text = valid === true ? 'valid' : (valid === false ? 'invalid' : 'initial');
+
+    return state;
+  }),
+
+  register: 'register',
 
   actions: {
     setState(params) {
@@ -13,4 +35,5 @@ export default Ember.Component.extend({
       this.set('valid', params.valid);
     }
   }
+
 });
