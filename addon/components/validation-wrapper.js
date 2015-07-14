b149db2501ed7313bd9fd255c1958aa3e51ee57c
Seth Pollack <seth@sethpollack.net>
2015-07-13 22:48:29 -0400

    clean up wrapper

--- addon/components/validation-wrapper.js
+++ addon/components/validation-wrapper.js
@@ -1,52 +1,27 @@
 import Ember from 'ember';
 import layout from '../templates/components/validation-wrapper';
 import ValidationBase from '../mixins/validation-base';
+import ValidatedArea from './validated-area';
 import runValidations from '../utils/run-validations';
 
 const {
-  computed,
-  Component,
   observer,
   on
   } = Ember;
 
-export default Component.extend(ValidationBase, {
-  layout: layout,
-
+export default ValidatedArea.extend(ValidationBase, {
   value: null,
-  _isWrapperComponent: true,
 
   _isInitialized: false,
   _setValueInitialization: on('didInsertElement', function() {
     this.set('_isInitialized', true);
   }),
+
   _runValidations: observer('value', function() {
     if (this.get('_isInitialized')) {
       this.send('validate');
     }
   }),
-
-  valid: false,
-  errors: null,
-
-  validationState: computed('valid', function() {
-    const valid = this.get('valid');
-    let state = {};
-
-    state.isValid = valid === true;
-    state.isInvalid = valid === false;
-    state.isInitial = valid === null;
-    state.text = valid === true ? 'valid' : (valid === false ? 'invalid' : 'initial');
-
-    return state;
-  }),
-
-  setState: function(params) {
-    this.setProperties({
-      errors: params.errors,
-      valid: params.valid
-    });
-  },
 
   actions: {
     checkForValid() {
@@ -83,5 +58,4 @@ export default Component.extend(ValidationBase, {
       });
     }
   }
-
 });
