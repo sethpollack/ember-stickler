406888b0fb248d2a5381b39af8afee9ef6ef314f
Chris Thoburn <runspired@gmail.com>
2015-07-13 19:31:04 -0500

    fix length check for arrays

--- app/validations/required.js
+++ app/validations/required.js
@@ -1,7 +1,7 @@
 export default {
 
   validate(value, messages) {
-    if (!value) {
+    if (!value || !value.length) {
       const message = this.getWithDefault('requiredMessage', 'This field is required');
       messages.addObject({ message: message });
       return false;
