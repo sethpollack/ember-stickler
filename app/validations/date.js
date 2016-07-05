31baaf85ac51423d27e0d68077a593fae1d084b4
jholt1 <jonathan.a.holt@gmail.com>
2016-07-05 02:33:53 +0100

    test

--- app/validations/date.js
+++ app/validations/date.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 export default Validation.create({
   validate(value, errors) {
     const result = moment(value, 'MM-DD-YYYY').isValid();
+    console.log(result, value);
     if (result) {
       const message = this.getWithDefault('dateMessage', 'Please enter a valid date.');
 
