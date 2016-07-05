75f19d7e05d906f2812532e64468d01e051871ee
jholt1 <jonathan.a.holt@gmail.com>
2016-07-05 11:46:38 +0100

    warnings

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -6,6 +6,7 @@ import config from './config/environment';
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
+Ember.deprecate = () => {};
 
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
