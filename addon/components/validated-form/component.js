bb18e755ab09de703b67737933b19ff8357ec15b
Abraham Itule <abraham@itule.me>
2017-03-09 16:25:29 +0000

    update

--- addon/components/validated-form/component.js
+++ addon/components/validated-form/component.js
@@ -43,7 +43,7 @@ export default Component.extend({
       isDefault = (state === 'default');
 
     return {
-      isDefault: state === 'default',
+      isDefault,
       isPending: state === 'pending',
       isResolved: state === 'resolved',
       isRejected: state === 'rejected',
